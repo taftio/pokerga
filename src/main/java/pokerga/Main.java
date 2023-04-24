a737e46f73be2fa03150e7d74a19f55eea715ef7
Adam Taft <adam@taftio.com>
2023-04-23 23:35:19 -0600

    Add maven project configuration

---
+++ src/main/java/pokerga/Main.java
@@ -0,0 +1,10 @@
+package pokerga;
+
+import java.util.Arrays;
+
+public final class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Main method: " + Arrays.toString(args));
+  }
+}
